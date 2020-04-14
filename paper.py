caba325c6c7fef9477ebc86e9f3789077b4e5f79
FReina <f.reina3@gmail.com>
2020-04-14 18:35:27 +0200

    Update 14/04

--- paper.py
+++ paper.py
@@ -1,22 +1,22 @@
-import nbformat
+#import nbformat
 from nbparameterise import (extract_parameters, replace_definitions,parameter_values)
 import notebooktoall.transform as trans
 
-with open("masso.ipynb") as f:
-    nb = nbformat.read(f,as_version=4)
+#with open("masso.ipynb") as f:
+#    nb = nbformat.read(f,as_version=4)
 
-orig_parameters = extract_parameters(nb)
+#orig_parameters = extract_parameters(nb)
 
-params = parameter_values(orig_parameters, filename = './dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv')
+#params = parameter_values(orig_parameters, filename = './dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv')
 
-new_nb = replace_definitions(nb, params)
+#new_nb = replace_definitions(nb, params)
 
-with open("masso2.ipynb","w") as f:
-    nbformat.write(new_nb,f)
+#with open("masso2.ipynb","w") as f:
+#    nbformat.write(new_nb,f)
 
 
-trans.transform_notebook('masso2.ipynb',export_list=['html'])
+trans.transform_notebook(".\\analysis_v0.2.ipynb",export_list=['html'])
 
-nodeof = trans.get_notebook('.\masso2.ipynb')
+#nodeof = trans.get_notebook('.\masso2.ipynb')
 
-trans.write_files(['html'],nodeof,'..\masso3.html')
+#trans.write_files(['html'],nodeof,'..\analysis_v0.2.html')
